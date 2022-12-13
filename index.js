21350a312235cc3500de90a94af3c0511096d865
eliad <eliad@papa.digital>
2022-12-13 19:11:05 +0200

    port value fix

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get('/scriptMain.js', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Express app listening on port 3000!')
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Express app listening on port ${process.env.SERVER_PORT}!`)
 });
