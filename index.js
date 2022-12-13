e6fc57e6367eafc4d5ac5839e1d1bb59ea71adb4
eliad <eliad@papa.digital>
2022-12-13 18:38:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// https://ui.invisiblesport.com/api/v2/auth/brand/1928067857632071680/settings?lang=en
+// geo value
+
+// require express
+
+const express = require('express');
+const request = require('request');
+const app = express();
+
+const sslPrefix = process.env.SSL ? 'https://' : 'http://';
+
+require('dotenv').config();
+
+// rtret
+app.get('/', (req, res) => {
+
+    request(process.env.SPORT_URL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+          body = body.replace(
+            '//d2en6kvan5r1zc.cloudfront.net/Assets/Bundle/Webpack/Scripts/BourneGreen/scriptMain.js?awlav=10',
+            `${sslPrefix}${process.env.SERVER_URL}/scriptMain.js`
+          );
+
+            extensions = '<style>body{margin:0;padding:0;}</style>'
+            body += extensions;
+
+            res.send(body);
+        }
+    });
+});
+
+/**
+ * @description
+ */
+app.get('/scriptMain.js', (req, res) => {
+    request(process.env.SCRIPT_URL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+            body = body.replace(
+                '/ApiSession/LogJavaScript?as=0',
+                'https://quote.usinsuranceonline.com/AutoSimple/ApiSession/LogJavaScript?as=0'
+            );
+
+            res.send(body);
+        }
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Express app listening on port 3000!')
+});
